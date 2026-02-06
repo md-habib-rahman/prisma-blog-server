effd9228bd0234c2ae2b5d9e1cea27f6a3f8ebd0
Md. Habibur Rahman Bappy <habib.rahman@live.com>
2026-02-07 01:58:42 +0600

    applied pagination in get routes

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,54 +4,54 @@ import { prisma } from "./prisma";
 import nodemailer from "nodemailer"
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // Use true for port 465, false for port 587
-  auth: {
-    user: process.env.APP_USER,
-    pass: process.env.APP_PASS,
-  },
+	host: "smtp.gmail.com",
+	port: 587,
+	secure: false, // Use true for port 465, false for port 587
+	auth: {
+		user: process.env.APP_USER,
+		pass: process.env.APP_PASS,
+	},
 });
 
 export const auth = betterAuth({
-  database: prismaAdapter(prisma, {
-    provider: "postgresql", // or "mysql", "postgresql", ...etc
-  }),
-  trustedOrigins: [process.env.APP_URL!],
-  user: {
-    additionalFields: {
-      role: {
-        type: "string",
-        defaultValue: "USER",
-        required: false
-      },
-      phone: {
-        type: "string",
-        required: false
-      },
-      status: {
-        type: "string",
-        defaultValue: "ACTIVE",
-        required: false
-      }
-    }
-  },
-  emailAndPassword: {
-    enabled: true,
-    autoSignIn: false,
-    requireEmailVerification: true
-  },
-  emailVerification: {
-    sendOnSignUp: true,
-    autoSignInAfterVerification: true,
-    sendVerificationEmail: async ({ user, url, token }, request) => {
-      try {
-        const verificationUrl = `${process.env.APP_URL}/verify-email?token=${token}`
-        const info = await transporter.sendMail({
-          from: '"Prisma Blog" <prismablog@ph.com>',
-          to: user.email,
-          subject: "Please verify your email!",
-          html: `<!DOCTYPE html>
+	database: prismaAdapter(prisma, {
+		provider: "postgresql", // or "mysql", "postgresql", ...etc
+	}),
+	trustedOrigins: [process.env.APP_URL!],
+	user: {
+		additionalFields: {
+			role: {
+				type: "string",
+				defaultValue: "USER",
+				required: false
+			},
+			phone: {
+				type: "string",
+				required: false
+			},
+			status: {
+				type: "string",
+				defaultValue: "ACTIVE",
+				required: false
+			}
+		}
+	},
+	emailAndPassword: {
+		enabled: true,
+		autoSignIn: false,
+		requireEmailVerification: true
+	},
+	emailVerification: {
+		sendOnSignUp: true,
+		autoSignInAfterVerification: true,
+		sendVerificationEmail: async ({ user, url, token }, request) => {
+			try {
+				const verificationUrl = `${process.env.APP_URL}/verify-email?token=${token}`
+				const info = await transporter.sendMail({
+					from: '"Prisma Blog" <prismablog@ph.com>',
+					to: user.email,
+					subject: "Please verify your email!",
+					html: `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8" />
@@ -181,24 +181,24 @@ export const auth = betterAuth({
 </body>
 </html>
 `
-        });
+				});
 
-        console.log("Message sent:", info.messageId);
-      } catch (err) {
-        console.error(err)
-        throw err;
-      }
-    },
-  },
+				console.log("Message sent:", info.messageId);
+			} catch (err) {
+				console.error(err)
+				throw err;
+			}
+		},
+	},
 
-  socialProviders: {
-    google: {
-      prompt: "select_account consent",
-      accessType: "offline",
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    },
-  },
+	socialProviders: {
+		google: {
+			prompt: "select_account consent",
+			accessType: "offline",
+			clientId: process.env.GOOGLE_CLIENT_ID as string,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+		},
+	},
 });
 
 
--- src/scripts/seedAdmin.ts
+++ src/scripts/seedAdmin.ts
@@ -2,54 +2,55 @@ import { prisma } from "../lib/prisma";
 import { UserRole } from "../middlewares/auth";
 
 async function seedAdmin() {
-    try {
-        console.log("***** Admin Seeding Started....")
-        const adminData = {
-            name: "Admin2 Saheb",
-            email: "admin2@admin.com",
-            role: UserRole.ADMIN,
-            password: "admin1234"
-        }
-        console.log("***** Checking Admin Exist or not")
-        // check user exist on db or not
-        const existingUser = await prisma.user.findUnique({
-            where: {
-                email: adminData.email
-            }
-        });
+	try {
+		console.log("***** Admin Seeding Started....")
+		const adminData = {
+			name: "Admin2 Saheb",
+			email: "admin2@admin.com",
+			role: UserRole.ADMIN,
+			password: "admin1234"
+		}
+		console.log("***** Checking Admin Exist or not")
+		// check user exist on db or not
+		const existingUser = await prisma.user.findUnique({
+			where: {
+				email: adminData.email
+			}
+		});
 
-        if (existingUser) {
-            throw new Error("User already exists!!");
-        }
+		if (existingUser) {
+			throw new Error("User already exists!!");
+		}
 
-        const signUpAdmin = await fetch("http://localhost:3000/api/auth/sign-up/email", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(adminData)
-        })
+		const signUpAdmin = await fetch("http://localhost:3000/api/auth/sign-up/email", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				"Origin": "http://localhost:4000"
+			},
+			body: JSON.stringify(adminData)
+		})
 
 
+		// console.log(signUpAdmin)
+		if (signUpAdmin.ok) {
+			console.log("**** Admin created")
+			await prisma.user.update({
+				where: {
+					email: adminData.email
+				},
+				data: {
+					emailVerified: true
+				}
+			})
 
-        if (signUpAdmin.ok) {
-            console.log("**** Admin created")
-            await prisma.user.update({
-                where: {
-                    email: adminData.email
-                },
-                data: {
-                    emailVerified: true
-                }
-            })
+			console.log("**** Email verification status updated!")
+		}
+		console.log("******* SUCCESS ******")
 
-            console.log("**** Email verification status updated!")
-        }
-        console.log("******* SUCCESS ******")
-
-    } catch (error) {
-        console.error(error);
-    }
+	} catch (error) {
+		console.error(error);
+	}
 }
 
 seedAdmin()
